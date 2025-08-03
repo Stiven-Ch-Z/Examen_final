f0478d9a1810c66592577f491e0feea91dfc98a8
Stiven <Stiven@DESKTOP-4CA2HN8>
2025-08-03 12:17:53 -0600

    se  comenzo y termino el ejercicio 3

---
+++ Examen_final/Entidades/EleccionJugador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_final.Entidades
+{
+    public class EleccionJugador
+    {
+        public string Eleccion {  get; set; }  //valor de la clase
+
+        public  EleccionJugador (string eleccion)//constructor de la clase
+        {
+            Eleccion = eleccion; //parametros de los valores de la clase
+        }
+    }
+}
